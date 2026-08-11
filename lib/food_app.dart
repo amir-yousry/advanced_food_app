4472617bfc2fc3a7b4c31354e123b58d480ae57a
Amir Yousry <mr.amiryousry@gmail.com>
2026-08-11 15:19:43 +0300

    chore: organize project folder structure
    Removes legacy app structure and integrates new routing
    
    Eliminates the previous application structure, including the main app and home page components.
    
    Integrates a new entry point using the `FoodApp` with an `AppRouter`, streamlining the app's routing capabilities.
    
    This change prepares the application for future enhancements and improves code maintainability.

---
+++ lib/food_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/core/routing/app_router.dart';
+import 'package:food_app/core/routing/routes.dart';
+
+class FoodApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const FoodApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food App',
+      debugShowCheckedModeBanner: false,
+      //initialRoute: isLoggedInUser ? Routes.homeScreen : Routes.loginScreen,
+      initialRoute: Routes.loginScreen,
+      onGenerateRoute: appRouter.generateRoute,
+    );
+  }
+}
