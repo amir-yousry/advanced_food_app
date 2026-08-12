9c6cc931aa4fafd3acfa67dc850ed8877b720f3d
Amir Yousry <mr.amiryousry@gmail.com>
2026-08-12 12:55:59 +0300

    Refactors order history screen imports
    Updates imports for the order history screen to reflect a change in directory naming from `order_history` to `orders_history`.
    
    Removes the obsolete `OrderHistoryScreen` implementation, streamlining the codebase and improving maintainability.

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -3,7 +3,7 @@ import 'package:food_app/core/routes/routes.dart';
 import 'package:food_app/feature/cart/ui/cart_screen.dart';
 import 'package:food_app/feature/checkout/ui/checkout_screen.dart';
 import 'package:food_app/feature/auth/ui/login_screen.dart';
-import 'package:food_app/feature/order_history/ui/order_history_screen.dart';
+import 'package:food_app/feature/orders_history/ui/order_history_screen.dart';
 import 'package:food_app/feature/product_details/ui/product_details_screen.dart';
 import 'package:food_app/feature/profile/ui/profile_screen.dart';
 import 'package:food_app/feature/auth/ui/sign_up_screen.dart';
