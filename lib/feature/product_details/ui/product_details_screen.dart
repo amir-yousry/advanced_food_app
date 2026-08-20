a868f716a494b4ff53010663d00989052c5b9cd5
Amir Yousry <mr.amiryousry@gmail.com>
2026-08-20 20:25:40 +0300

    Enables scrolling for product details screen
    Wraps the main content of the product details screen in a `SingleChildScrollView`. This ensures all elements remain accessible and prevents overflow issues on devices with varying screen sizes.

--- lib/feature/product_details/ui/product_details_screen.dart
+++ lib/feature/product_details/ui/product_details_screen.dart
@@ -17,186 +17,192 @@ class ProductDetailsScreen extends StatelessWidget {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Image.asset('assets/detail/sandwitch_detail.png', height: 250),
-                const Spacer(),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RichText(
-                      text: const TextSpan(
-                        text: 'Customize',
-                        style: TextStyle(
-                          fontSize: 13,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                        children: [
-                          TextSpan(
-                            text:
-                                ' Your Burger\nto Your Tastes.\nUltimate Experience',
-                            style: TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.black,
-                            ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.zero,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/detail/sandwitch_detail.png',
+                    height: 250,
+                  ),
+                  const Spacer(),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          text: 'Customize',
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
-                        ],
-                      ),
-                    ),
-                    Gap(5),
-                    SizedBox(
-                      width: 150,
-                      child: Slider(
-                        min: 0,
-                        max: 1,
-                        value: 1,
-                        onChanged: (value) {},
-                        inactiveColor: Colors.grey.shade300,
-                        activeColor: AppColors.primary,
-                      ),
-                    ),
-                  ],
-                ),
-                Gap(10),
-              ],
-            ),
-            const Gap(20),
-
-            /// Toppings
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List.generate(5, (index) {
-                  return Stack(
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text('Topping ${index + 1}'),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 150,
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 5,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Row(
                           children: [
-                            Text('Topping ${index + 1}'),
-                            const Spacer(),
-                            IconButton(
-                              icon: const Icon(Icons.add),
-                              onPressed: () {
-                                // Handle add topping
-                              },
+                            TextSpan(
+                              text:
+                                  ' Your Burger\nto Your Tastes.\nUltimate Experience',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                              ),
                             ),
                           ],
                         ),
                       ),
+                      Gap(5),
+                      SizedBox(
+                        width: 150,
+                        child: Slider(
+                          min: 0,
+                          max: 1,
+                          value: 1,
+                          onChanged: (value) {},
+                          inactiveColor: Colors.grey.shade300,
+                          activeColor: AppColors.primary,
+                        ),
+                      ),
                     ],
-                  );
-                }),
+                  ),
+                  Gap(10),
+                ],
               ),
-            ),
-            const Gap(20),
+              const Gap(20),
 
-            /// Side Options
-            SizedBox(
-              height: 100,
-              child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Stack(
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.circular(10),
+              /// Toppings
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(5, (index) {
+                    return Stack(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text('Topping ${index + 1}'),
                         ),
-                        child: Text('Topping ${index + 1}'),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 150,
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 5,
+                        Container(
+                          width: 100,
+                          height: 150,
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 5,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              Text('Topping ${index + 1}'),
+                              const Spacer(),
+                              IconButton(
+                                icon: const Icon(Icons.add),
+                                onPressed: () {
+                                  // Handle add topping
+                                },
+                              ),
+                            ],
+                          ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Row(
-                          children: [
-                            Text('Topping ${index + 1}'),
-                            const Spacer(),
-                            IconButton(
-                              icon: const Icon(Icons.add),
-                              onPressed: () {
-                                // Handle add topping
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
-            const Gap(20),
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Total Price',
-                      style: TextStyle(fontSize: 16, color: Colors.grey),
-                    ),
-                    Gap(5),
-                    Text(
-                      '\$10.99',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
+                      ],
+                    );
+                  }),
                 ),
-                Spacer(),
-                ElevatedButton(
-                  onPressed: () {
-                    // Handle add to cart
+              ),
+              const Gap(20),
+
+              /// Side Options
+              SizedBox(
+                height: 100,
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Stack(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text('Topping ${index + 1}'),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 150,
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 5,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              Text('Topping ${index + 1}'),
+                              const Spacer(),
+                              IconButton(
+                                icon: const Icon(Icons.add),
+                                onPressed: () {
+                                  // Handle add topping
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
                   },
-                  child: const Text('Add to Cart'),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const Gap(20),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Total Price',
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                      Gap(5),
+                      Text(
+                        '\$10.99',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle add to cart
+                    },
+                    child: const Text('Add to Cart'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
